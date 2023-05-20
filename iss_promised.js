83436a3b6e8f2277e8a8697209e19a76b30d6dfe
Ryan Stewart <ryanstew17@gmail.com>
2023-05-20 14:39:06 -0300

    refactor code with promisies

---
+++ iss_promised.js
@@ -0,0 +1,14 @@
+// iss_promised.js
+const request = require('request-promise-native');
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
